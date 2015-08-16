60a78b9060eb0d31dfc7f9380bcd3aa222565439
hallchen8 <hall@hallchen.us>
2015-08-15 23:45:58 -0700

    added genre searchability; able to do new search without refreshing page; renamed HTML keyword input box to tag to avoid confusion

--- idtools.js
+++ idtools.js
@@ -1,3 +1,4 @@
+
 function getPeopleID(data){
         var arrayData = JSON.parse(data);
         console.log("Actor ID:" + arrayData["results"]["0"]["id"]);
@@ -19,4 +20,55 @@ function successCB(data) {
 
 function errorCB(data) {
             console.log("Error callback: " + data);
+}
+
+function isGenre(tag){
+        var genres = {
+                "action":28,
+                 "adventure":12,
+                 "animation":16,
+                 "animated":16,
+                 "cartoon":16,
+                 "comedy":35,
+                 "funny":35,
+                 "humor":35,
+                 "humorous":35,
+                 "crime":80,
+                 "police":80,
+                 "documentary":99,
+                 "nonfiction":99,
+                 "non-fiction":99,
+                 "non fiction":99,
+                 "drama":18,
+                 "family":10751,
+                 "kids":10751,
+                 "kid":10751,
+                 "child":10751,
+                 "children":10751,
+                 "fantasy":14,
+                 "foreign":10769,
+                 "history":36,
+                 "horror":27,
+                 "scary":27,
+                 "music":10402,
+                 "mystery":9648,
+                 "romance":10749,
+                 "love":10749,
+                 "science fiction":878,
+                 "scifi":878,
+                 "sci-fi":878,
+                 "sci fi":878,
+                 "tv":10770,
+                 "thriller":53,
+                 "war":10752,
+                 "battle":10752,
+                 "military":10752,
+                 "western":37
+        }
+
+        return genres[tag];
+}
+
+function empty(array){
+        array = array.splice(0,array.length);
 }
